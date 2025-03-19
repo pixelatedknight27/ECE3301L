981f86590a2bfa3186235035c587d2cb1878f82e
Joshhua1883 <jgnavarrete@cpp.edu>
2025-03-18 18:38:45 -0700

    done up til task 9 on handout

--- LAB8/Lab8p1.X/Lab8_S1_sample.c
+++ LAB8/Lab8p1.X/Lab8_S1_sample.c
@@ -19,10 +19,10 @@ void Set_NS(char color);
 void Set_NS_LT(char color);
 void Set_EW(char color);
 void Set_EW_LT(char color);
-void PED_Control( char Direction, char Num_Sec);
+void PED_Control(char Direction, char Num_Sec);
 void Day_Mode();
 void Night_Mode();
-void Wait_N_Seconds (char);
+void Wait_N_Seconds(char);
 void Wait_One_Second_With_Beep();
 void Wait_One_Second();
 
@@ -31,126 +31,178 @@ void Wait_One_Second();
 #define TMR_CLOCK   _XTAL_FREQ/4        // Timer Clock 2 Mhz
 
 char dir;
-char Count;                             // RAM variable for Second Count
-char PED_Count;                         // RAM variable for Second Pedestrian Count
+char Count; // RAM variable for Second Count
+char PED_Count; // RAM variable for Second Pedestrian Count
 
-char  MODE;
+char MODE;
 char direction;
 float volt;
 
 extern char Light_Sensor;
 
-void main(void)
-{
-    OSCCON = 0x70;                      // set the system clock to be 1MHz 1/4 of the 4MHz
-    TRISA = 0x1F;                       //inputs RA0/AN0-RA4 inputs,RA5 output
-    TRISB = 0x07;                       // outputs
-    TRISC = 0x00;                       // set PORTC as outputs
-    TRISD = 0x00;                       // set PORTD as outputs
-    TRISE = 0x00;                       // set PORTE as outputs
+void main(void) {
+    OSCCON = 0x70; // set the system clock to be 1MHz 1/4 of the 4MHz
+    TRISA = 0x1F; //inputs RA0/AN0-RA4 inputs,RA5 output
+    TRISB = 0x07; // outputs
+    TRISC = 0x00; // set PORTC as outputs
+    TRISD = 0x00; // set PORTD as outputs
+    TRISE = 0x00; // set PORTE as outputs
     Init_ADC();
     Init_UART();
 
-    Initialize_LCD_Screen();                        // Initialize the TFT screen
+    Initialize_LCD_Screen(); // Initialize the TFT screen
 
-//    while (1)
-//    {
-//        Rcmd2red();
-//        delay_ms(2);
-//        
-//    }
+    while (1) {
+        Rcmd2red();
+        delay_ms(2);
 
-    volt = Read_Volt(0);                        // 
+    }
 
-    Light_Sensor = volt < 2.5 ? 1:0;                 // Mode = 1, Day_mode, Mode = 0 Night_mode
+    volt = Read_Volt(0); // 
 
-    while(1)                                    // forever loop
+    Light_Sensor = volt < 2.5 ? 1 : 0; // Mode = 1, Day_mode, Mode = 0 Night_mode
+
+    while (1) // forever loop
     {
 
-        if (Light_Sensor == 1)    
-        {
-            Day_Mode();                         // calls Day_Mode() function
+        if (Light_Sensor == 1) {
+            Day_Mode(); // calls Day_Mode() function
+        } else {
+            Night_Mode(); // calls Night_Mode() function
         }
-        else
-        {
-            Night_Mode();                       // calls Night_Mode() function
-        }
-     
-    } 
+
+    }
 }
 
+void Set_NS(char color) {
+    direction = NS;
+    update_LCD_color(direction, color);
 
-void Set_NS(char color)
-{
+    switch (color) {
+        case OFF: NS_RED = 0;
+            NS_GREEN = 0;
+            break; // Turns off the NS LED
+        case RED: NS_RED = 1;
+            NS_GREEN = 0;
+            break; // Sets NS LED RED
+        case GREEN: NS_RED = 0;
+            NS_GREEN = 1;
+            break; // sets NS LED GREEN
+        case YELLOW: NS_RED = 1;
+            NS_GREEN = 1;
+            break; // sets NS LED YELLOW
+    }
     // add code here
 }
 
-void Set_NS_LT(char color)
-{
+void Set_NS_LT(char color) {
+    direction = NSLT;
+    update_LCD_color(direction, color);
+
+    switch (color) {
+        case OFF: NSLT_RED = 0;
+            NSLT_GREEN = 0;
+            break; // Turns off the NS LED
+        case RED: NSLT_RED = 1;
+            NSLT_GREEN = 0;
+            break; // Sets NS LED RED
+        case GREEN: NSLT_RED = 0;
+            NSLT_GREEN = 1;
+            break; // sets NS LED GREEN
+        case YELLOW: NSLT_RED = 1;
+            NSLT_GREEN = 1;
+            break; // sets NS LED YELLOW
+    }
     // add code here
 }
 
-void Set_EW(char color)
-{
+void Set_EW(char color) {
+    direction = EW;
+    update_LCD_color(direction, color);
+
+    switch (color) {
+        case OFF: EW_RED = 0;
+            EW_GREEN = 0;
+            break; // Turns off the NS LED
+        case RED: EW_RED = 1;
+            EW_GREEN = 0;
+            break; // Sets NS LED RED
+        case GREEN: EW_RED = 0;
+            EW_GREEN = 1;
+            break; // sets NS LED GREEN
+        case YELLOW: EW_RED = 1;
+            EW_GREEN = 1;
+            break; // sets NS LED YELLOW
+    }
     // add code here
 }
 
-void Set_EW_LT(char color)
-{
+void Set_EW_LT(char color) {
+    direction = EWLT;
+    update_LCD_color(direction, color);
+
+    switch (color) {
+        case OFF: EWLT_RED = 0;
+            EWLT_GREEN = 0;
+            break; // Turns off the NS LED
+        case RED: EWLT_RED = 1;
+            EWLT_GREEN = 0;
+            break; // Sets NS LED RED
+        case GREEN: EWLT_RED = 0;
+            EWLT_GREEN = 1;
+            break; // sets NS LED GREEN
+        case YELLOW: EWLT_RED = 1;
+            EWLT_GREEN = 1;
+            break; // sets NS LED YELLOW
+    }
     // add code here
 }
 
-void PED_Control( char direction, char Num_Sec)
-{ 
+void PED_Control(char direction, char Num_Sec) {
     // add code here
 }
 
-void Day_Mode()
-{
+void Day_Mode() {
     // add code here
 }
 
-void Night_Mode()
-{ 
+void Night_Mode() {
     // add code here
 }
 
-void Wait_One_Second()							//creates one second delay and blinking asterisk
+void Wait_One_Second() //creates one second delay and blinking asterisk
 {
     SEC_LED = 1;
     Draw_Star();
-    Wait_Half_Second();                         // Wait for half second (or 500 msec)
+    Wait_Half_Second(); // Wait for half second (or 500 msec)
     SEC_LED = 0;
     Erase_Star();
-    Wait_Half_Second();                         // Wait for half second (or 500 msec)
-	// add code here
+    Wait_Half_Second(); // Wait for half second (or 500 msec)
+    // add code here
 }
 
-void Wait_One_Second_With_Beep()				//creates one second delay as well as sound buzzer
+void Wait_One_Second_With_Beep() //creates one second delay as well as sound buzzer
 {
     SEC_LED = 1;
     Draw_Star();
     Activate_Buzzer();
-    Wait_Half_Second();                         // Wait for half second (or 500 msec)
+    Wait_Half_Second(); // Wait for half second (or 500 msec)
 
     SEC_LED = 0;
     Erase_Star();
     Deactivate_Buzzer();
-    Wait_Half_Second();                         // Wait for half second (or 500 msec)
-	// add code here;
+    Wait_Half_Second(); // Wait for half second (or 500 msec)
+    // add code here;
 }
- 
 
-void Wait_N_Seconds (char seconds)
-{
+void Wait_N_Seconds(char seconds) {
     char I;
-    for (I = seconds; I> 0; I--)
-    {
-		// add code here;
-        Wait_One_Second();          			// calls Wait_One_Second for x number of times
-        
+    for (I = seconds; I > 0; I--) {
+        // add code here;
+        Wait_One_Second(); // calls Wait_One_Second for x number of times
+
     }
-	// add code here;
+    // add code here;
 }
 
 
