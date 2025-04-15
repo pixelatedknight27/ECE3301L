2c735cbff719c2c198f4c76dbb3356788b377427
pixelatedknight27 <maxgross72@gmail.com>
2025-04-15 15:47:38 -0700

    Lab 11
    can get temp, can read sec

---
+++ LAB11/Lab11p1.X/src/I2C_Support.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#include <p18f4620.h>
+#include "I2C_Support.h"
+#include "I2C.h"
+
+#define ACCESS_CFG      0xAC
+#define START_CONV      0xEE
+#define READ_TEMP       0xAA
+#define CONT_CONV       0x02
+#define ACK             1
+#define NAK             0
+
+#define DS1621_ADDR     0x48
+#define DS3231_ADDR     0x68
+
+
+
+extern unsigned char second, minute, hour, dow, day, month, year;
+extern unsigned char setup_second, setup_minute, setup_hour, setup_day, setup_month, setup_year;
+extern unsigned char alarm_second, alarm_minute, alarm_hour, alarm_date;
+extern unsigned char setup_alarm_second, setup_alarm_minute, setup_alarm_hour;
+
+extern char I2C_Write_Cmd_Read_One_Byte(char Device, char Cmd);
+
+int DS1621_Read_Temp()
+{
+    return (I2C_Write_Cmd_Read_One_Byte (DS1621_ADDR, READ_TEMP));
+}
+
+void DS1621_Init()
+{
+    I2C_Write_Cmd_Write_Data (DS1621_ADDR, ACCESS_CFG, CONT_CONV);
+    I2C_Write_Cmd_Only(DS1621_ADDR, START_CONV);
+}
+
+void DS3231_Read_Time()
+{
+    second = I2C_Write_Address_Read_One_Byte (DS3231_ADDR, 0x00, ACK);
+    minute = I2C_Write_Address_Read_One_Byte (DS3231_ADDR, 0x01, ACK);
+    hour = I2C_Write_Address_Read_One_Byte (DS3231_ADDR, 0x02, ACK);
+    dow = I2C_Write_Address_Read_One_Byte (DS3231_ADDR, 0x03, ACK);
+    day = I2C_Write_Address_Read_One_Byte (DS3231_ADDR, 0x04, ACK);
+    month = I2C_Write_Address_Read_One_Byte (DS3231_ADDR, 0x05, ACK);
+    year = I2C_Write_Address_Read_One_Byte (DS3231_ADDR, 0x06, ACK);
+}
+
+void DS3231_Setup_Time()
+{
+    I2C_Write_Address_Write_One_Byte(DS3231_ADDR, 0x00, second);
+    I2C_Write_Address_Write_One_Byte(DS3231_ADDR, 0x01, minute);
+    I2C_Write_Address_Write_One_Byte(DS3231_ADDR, 0x02, hour);
+    I2C_Write_Address_Write_One_Byte(DS3231_ADDR, 0x03, dow);
+    I2C_Write_Address_Write_One_Byte(DS3231_ADDR, 0x04, day);
+    I2C_Write_Address_Write_One_Byte(DS3231_ADDR, 0x05, month);
+    I2C_Write_Address_Write_One_Byte(DS3231_ADDR, 0x06, year);
+
+}
+
