c6f4482eb3e7f216b7c7b26700e9de355e3ab72d
pixelatedknight27 <maxgross72@gmail.com>
2025-04-22 19:23:33 -0700

    renamed stuff

--- LAB12/Lab12pA.X/nbproject/Package-default.bash
+++ LAB12/Lab12pA.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Lab12p1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Lab12p1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=lab12p1.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Lab12pA.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=Lab12pA.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=lab12pa.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/lab12p1.x/bin
+makeDirectory ${TMPDIR}/lab12pa.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/lab12p1.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/lab12pa.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/lab12p1.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/lab12pa.x.tar *
 checkReturnCode
 
 # Cleanup
